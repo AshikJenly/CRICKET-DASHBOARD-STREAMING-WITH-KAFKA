207a0f91fdb1f951d5599798d5069f04aded2609
AshikJenly <jenlyashik@gmail.com>
2023-11-21 23:50:26 +0530

    Added DB schema

---
+++ DataBase/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE cricket_match_data (
+    match_id INT,
+    inning INT,
+    batting_team VARCHAR(255),
+    bowling_team VARCHAR(255),
+    over INT,
+    ball INT,
+    batsman VARCHAR(255),
+    non_striker VARCHAR(255),
+    bowler VARCHAR(255),
+    is_super_over INT,
+    wide_runs INT,
+    bye_runs INT,
+    legbye_runs INT,
+    noball_runs INT,
+    penalty_runs INT,
+    batsman_runs INT,
+    extra_runs INT,
+    total_runs INT,
+    player_dismissed VARCHAR(255),
+    dismissal_kind VARCHAR(255),
+    fielder VARCHAR(255)
+);
