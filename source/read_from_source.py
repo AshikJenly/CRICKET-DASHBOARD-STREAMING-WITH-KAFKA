a9a01100eb15a07b042246fd16005743937db997
AshikJenly <jenlyashik@gmail.com>
2023-11-22 01:14:00 +0530

    updated reading data code

---
+++ source/read_from_source.py
@@ -0,0 +1,49 @@
+from confluent_kafka import Consumer, KafkaError
+import json
+import time
+import threading
+
+conf = {
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'cricket_group_a',
+    'auto.offset.reset': 'earliest',
+    'enable.auto.commit': True,
+    'auto.commit.interval.ms': 1000,
+}
+
+consumer = Consumer(conf)
+consumer.subscribe(['mysql.cricket_match_data_live'])
+
+
+shared_data = []
+
+def get_data(shared_data):
+    try:
+        print("Started consuming")
+        while True:
+            msg = consumer.poll(0.8)
+            if msg is None:
+                print("waiting")
+                continue
+            elif msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    continue
+                else:
+                    print(msg.error())
+                    break
+            else:
+                byte_data = msg.value()
+                str_data = byte_data.decode('utf-8')
+                my_dict = json.loads(str_data)
+                payload = my_dict["payload"]
+                shared_data.append(payload)
+                print("appending")
+    except KeyboardInterrupt:
+        consumer.close() 
+    finally:
+        print("closed consumer")
+        consumer.close()
+
+# Start Kafka consumer thread
+consumer_thread = threading.Thread(target=get_data, args=(shared_data,))
+consumer_thread.start()
