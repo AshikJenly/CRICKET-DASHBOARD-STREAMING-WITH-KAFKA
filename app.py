a0944aa01013fff40ebaa23fad9b85f181ce10c8
AshikJenly <jenlyashik@gmail.com>
2023-11-26 23:47:07 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from source.process_data import get_team_details, get_runs_per_over,get_wickets_per_over
+import time
+
+from source.process_data import (
+    get_team_details,
+    get_runs_per_over,
+    get_wickets_per_over,
+    get_striker_and_n_s,
+    get_batting_team,
+    get_bowler,
+    get_balls_faced,
+    get_bowling_team
+    )
 from itertools import zip_longest
 
-# Heading
-import time
+st.set_page_config(layout="wide")
 
 team_details = get_team_details()
 title = st.empty()
 team = st.empty()
+st.markdown("<hr style='border:2px solid white'>", unsafe_allow_html=True)
 
-col1, col2 = st.columns(2)
-with col1:
-    c1_h = st.empty()
-    c1_h.header("Column 1")
-    st.text("Content for column 1")
+batting, bowling = st.columns(2)
+with batting:
+    batting_team = st.empty()
+    ba1 = st.empty()
+    ba2 = st.empty()
 
 # Add content to the second column
-with col2:
-    c2_h = st.empty()
-    c2_h.header("Column 2")
-    st.text("Content for column 2")
+with bowling:
+    bowling_team = st.empty()
+    bowler = st.empty()
+st.markdown("<hr style='border:2px solid white'>", unsafe_allow_html=True)
 
+st.markdown("<h3 style='text-align:center'>Score Graph</h3>",unsafe_allow_html=True)
+line_chart = st.empty()
 Team_A_run = st.empty()
 Team_B_run = st.empty()
-line_chart = st.empty()
 
 # batsman
 # Create a 2-column layout
@@ -35,43 +47,55 @@ line_chart = st.empty()
 
 try:
     while True:
+        
         team_details = get_team_details()
 
         if team_details is not None:
 
             Team_A = team_details[1][0]
             Team_B = team_details[1][1]
-            title.title(f"Match No : {team_details[0]}")
             team.title(f"{Team_A}  vs {Team_B}")
 
             # batting.write(f"Batting Score of {Team_A}")
-            runs = get_runs_per_over(Team_A)
-            runs_2 = get_runs_per_over(Team_B)
+            team_a_runs = get_runs_per_over(Team_A)
+            team_b_runs = get_runs_per_over(Team_B)
             a_wickets = get_wickets_per_over(Team_A)
             b_wickets = get_wickets_per_over(Team_B)
     
             # Pad the shorter list with np.nan
-            # if runs_2 is not None:
-            padded_data = list(zip_longest(runs, runs_2, fillvalue=None))
+            # if team_b_runs is not None:
+            padded_data = list(zip_longest(team_a_runs, team_b_runs, fillvalue=None))
             x_padded, y_padded = zip(*padded_data)
-            runs_padded = list(x_padded)
-            runs_2_padded = list(y_padded)
+            team_a_runs_padded = list(x_padded)
+            team_b_runs_padded = list(y_padded)
             
-            c1_h.header(Team_A)
-            c2_h.header(Team_B)
+            
+            #Batting Details
+            try:
+                batting_team.write(f"{get_batting_team()} : {team_a_runs[-1]}/{len(a_wickets)}\t({len(team_a_runs)})")
+                bowling_team.write(f"{get_bowling_team()}")
+                striker,non_striker = get_striker_and_n_s(get_batting_team())
+                ba1.write(f"{striker[0]} - {sum(striker[1])} ({get_balls_faced(striker[0])} balls)")
+                ba2.write(f"{non_striker[0]} - {sum(non_striker[1])} ({get_balls_faced(non_striker[0])} balls)")
+                bowler.write(get_bowler())
+            except:
+                pass
+            
             Team_A_run.write(
-                f"{Team_A} : {runs[-1]}/{len(a_wickets)}\t({len(runs)})")
-            if len(runs_2)!=0:
-                Team_B_run.write(f"{Team_B} :{runs_2[-1]}/{len(b_wickets)}\t({len(runs_2)})")
+                f"{Team_A} : {team_a_runs[-1]}/{len(a_wickets)}\t({len(team_a_runs)})")
+            if len(team_b_runs)!=0:
+                Team_B_run.write(f"{Team_B} :{team_b_runs[-1]}/{len(b_wickets)}\t({len(team_b_runs)})")                
             else:
                 Team_B_run.write(f"{Team_B} : Not Yet Batted")
             df = pd.DataFrame({
-                Team_A: runs_padded,
-                Team_B: runs_2_padded,
+                Team_A: team_a_runs_padded,
+                Team_B: team_b_runs_padded,
             })
             # Plot the lines
             line_chart.line_chart(df, use_container_width=True)
             time.sleep(2)
+            title.title("JEN.live")
+            
         else:
             title.title("Loading Please Wait")
 except TypeError:
